42ede9b7d9cf2bd41012e2ffe9d529598326c3ba
Jacky <jackylai87@gmail.com>
2016-01-25 19:14:43 +0800

    Done styling and validating signin n signup page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,10 +3,10 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :avatar)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :avatar, :remote_avatar_url)
   end
 
   def account_update_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password, :avatar, :remove_avatar, :avatar_cache, :remote_avatar_url)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password, :avatar, :remove_avatar, :remote_avatar_url)
   end
 end
