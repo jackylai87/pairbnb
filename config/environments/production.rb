f26b03c62343c52a7faacebbbbdebb930cf2316d
Jacky <jackylai87@gmail.com>
2016-02-05 13:28:32 +0800

    Added a new host to herokuapp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = {:host => "localhost:3000"}
+  config.action_mailer.default_url_options = { host: 'jackybnb.herokuapp.com' }
+  # config.action_mailer.default_url_options = {:host => "localhost:3000"}
     config.action_mailer.smtp_settings = {
       :address              => "smtp.gmail.com",
       :port                 => 587,
@@ -88,5 +89,5 @@ Rails.application.configure do
       :password             => ENV['sitemail_password'],
       :authentication       => :plain,
       :enable_starttls_auto => true
-}
+    }
 end
