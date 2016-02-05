9273c4ef49171587778578d9e4e490b41353ff48
Jacky <jackylai87@gmail.com>
2016-02-05 13:12:42 +0800

    Done showing reservation and listing details in user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,6 @@ class UsersController < ApplicationController
 	
 	def show
 		@user = User.find(params[:id])
+		# @user_listing_reservations = Reservation.where("listing.user_id" == params[:id])
 	end
 end
